2ad952eb2ef169be2f981653ac8f3e813ce45660
Randall Nickerson <webshooter@gmail.com>
2019-12-07 15:07:23 -0800

    add seed value to makeChonk; move generator to builder

--- src/api/entities/chonk/chonk.test.js
+++ src/api/entities/chonk/chonk.test.js
@@ -1,3 +1,5 @@
+import uuidv5 from "uuid/v5";
+import uuidv4 from "uuid/v4";
 import faker from "faker";
 import buildMakeChonk from "./index";
 
@@ -5,43 +7,46 @@ const generateChonk = () => ("ABCD1234");
 
 describe("chonk", () => {
   const user = faker.random.uuid();
-  const namespace = "sample-namespace";
+  const namespace = uuidv5("jwtuserauth.example.com", uuidv5.DNS);
 
   it("creates a chonk", () => {
-    const makeChonk = buildMakeChonk({ user });
-    const chonk = makeChonk({
-      generateChonk,
-      namespace,
-    });
+    const makeChonk = buildMakeChonk({ generateChonk, namespace });
+    const seed = uuidv4();
+    const chonk = makeChonk({ user, seed });
 
     expect(chonk.getUser()).toBe(user);
     expect(chonk.getNamespace()).toBe(namespace);
+    expect(chonk.getSeed()).toBe(seed);
     expect(chonk.getStatus()).toBe("success");
     expect(chonk.getChonk()).toBe(generateChonk());
     expect(chonk.getTimestamp()).toBeLessThanOrEqual(Date.now());
   });
 
-  it("returns a status 'error' if chonk is not generated", () => {
-    const makeChonk = buildMakeChonk({ user });
-    const chonk = makeChonk({
-      generateChonk: () => (null),
-      namespace,
-    });
-
-    expect(chonk.getStatus()).toBe("error");
-  });
-
   it("throws an error if namespace is missing", () => {
-    const makeChonk = buildMakeChonk({ user });
+    const makeChonk = buildMakeChonk({ generateChonk, namespace: null });
 
-    expect(() => makeChonk({ generateChonk, namespace: null }))
+    expect(() => makeChonk({ user, seed: uuidv4() }))
       .toThrowError(new Error("Chonk requires a valid namespace"));
   });
 
-  it("throws an error if generator is not a function", () => {
-    const makeChonk = buildMakeChonk({ user });
+  it("throws an error if seed is missing", () => {
+    const makeChonk = buildMakeChonk({ generateChonk, namespace });
 
-    expect(() => makeChonk({ generateChonk: "string", namespace }))
+    expect(() => makeChonk({ user, seed: null }))
+      .toThrowError(new Error("Chonk requires a valid seed"));
+  });
+
+  it("throws an error if generator is not a function", () => {
+    const makeChonk = buildMakeChonk({ generateChonk: "not a function", namespace });
+
+    expect(() => makeChonk({ user, seed: uuidv4() }))
       .toThrowError(new Error("Chonk requires a valid generator"));
+  });
+
+  it("returns a status 'error' if chonk is not generated", () => {
+    const makeChonk = buildMakeChonk({ generateChonk: () => (null), namespace });
+    const chonk = makeChonk({ user, seed: uuidv4() });
+
+    expect(chonk.getStatus()).toBe("error");
   });
 });
--- src/api/entities/chonk/index.js
+++ src/api/entities/chonk/index.js
@@ -1,18 +1,23 @@
-const buildMakeChonk = ({ user } = {}) => ({ generateChonk, namespace }) => {
+const buildMakeChonk = ({ generateChonk, namespace } = {}) => ({ user, seed }) => {
   if (!namespace) {
     throw new Error("Chonk requires a valid namespace");
+  }
+
+  if (!seed) {
+    throw new Error("Chonk requires a valid seed");
   }
 
   if (!generateChonk || typeof generateChonk !== "function") {
     throw new Error("Chonk requires a valid generator");
   }
 
-  const chonk = generateChonk({ namespace });
+  const chonk = generateChonk({ namespace, seed, user });
   const timestamp = Date.now();
 
   return {
     getUser: () => user,
     getNamespace: () => namespace,
+    getSeed: () => seed,
     getChonk: () => chonk,
     getTimestamp: () => timestamp,
     getStatus: () => (chonk ? "success" : "error"),
