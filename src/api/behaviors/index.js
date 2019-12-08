6139f33c260ae8c9048c0655334ca3e9fcc56f79
Randall Nickerson <webshooter@gmail.com>
2019-12-08 15:41:02 -0800

    add index for behaviors

---
+++ src/api/behaviors/index.js
@@ -0,0 +1,8 @@
+import buildCreateChonk from "./create-chonk";
+
+const createChonk = buildCreateChonk();
+
+const behaviors = Object.freeze({ createChonk });
+
+export default behaviors;
+export { createChonk };
