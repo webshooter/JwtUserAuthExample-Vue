56b5b4c148223fd4e720f22bde8ca66facc0765b
Randall Nickerson <webshooter@gmail.com>
2019-12-07 16:31:30 -0800

    create entities index and add chonk to exports

---
+++ src/api/entities/index.js
@@ -0,0 +1,19 @@
+import uuidv5 from "uuid/v5";
+import buildMakeChonk from "./chonk";
+import buildGenerateChonkValue from "../generate-chonk-value";
+
+// eslint-disable-next-line import/named
+import { domain } from "../../config";
+
+const namespace = uuidv5(domain, uuidv5.DNS);
+
+const generateChonkValue = buildGenerateChonkValue({ namespace });
+
+const makeChonk = buildMakeChonk({ generateChonkValue, namespace });
+
+const entities = {
+  makeChonk,
+};
+
+export default entities;
+export { makeChonk };
